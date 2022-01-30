23da622e7d34ba9c9c70aec45473c03f73b4d980
Seth Scott <s.scottvancouverwa@gmail.com>
2022-01-30 09:01:39 -0800

    complete basic game functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import art
 import game_data
 import random
 
+# performs ranking calcs to establish winner, game logic
 def calcs(a, b, a_or_b):
     a_followers = a["follower_count"]
     b_followers = b["follower_count"]
@@ -12,35 +13,65 @@ def calcs(a, b, a_or_b):
     else:
         return "lose"
 
+# function that randomly selects an item from game_data (dictionary nested in a list)
+def randomizer():
+    return game_data.data[random.randint(0,len(game_data.data) -1)]
 
-
-# create function that randomly selects an item from game_data (dictionary nested in a list)
 print(art.logo)
 
+# running score
 score = 0
-while True:
-    option_a = game_data.data[random.randint(0,len(game_data.data))]
-    option_b = game_data.data[random.randint(0,len(game_data.data))]
+# is the user's first round? 
+first_round = True
+# are we still in a game or have we lost? 
+game = True
+# for the first round, sets option "a" to a random selection
+option_a = randomizer()
+
+while game:
+
+    # if the user passes the first round, option 'a' is set to the winner of the previous game
+    if first_round is False:
+        option_a = winner
+    option_b = randomizer()
+
+    # if the option 'a' and option 'b' randomly select the same item, re-shuffle item 'b' to a new item
     while option_a == option_b:
         option_b = game_data.data[random.randint(0,len(game_data.data))]
 
-    # Compare A: item, a widget, from Iraq.
+    # "Compare A: item, a widget, from Iraq.""
     print(f"Compare A: {option_a['name']}, a {option_a['description']}, from {option_a['country']}.")
-    # vs. art
+
     print(art.vs)
 
-    #  Against B: item, a widget, from Iran.
+    #  "Against B: item, a widget, from Iran."
     print(f"Against B: {option_b['name']}, a {option_b['description']}, from {option_b['country']}.")
 
-    #  Who has more followers? A or B? 
+    #  who has more followers? A or B? 
     user_selection = input("Who has more followers? 'a' or 'b'? ").lower()
 
+    # if you win, increase the score, set option 'a' to the previous round's winner.
+    if calcs(option_a, option_b, user_selection) == "win":
+        score += 1
+        first_round = False
+        if user_selection == "a":
+            winner = option_a
+            loser = option_b
+        if user_selection == "b":
+            loser = option_a
+            winner = option_b 
+
+    # if you lose, you lose
     if calcs(option_a, option_b, user_selection) == "lose":
         print("You LOSE!")
         print(f" SCORE: {score}")
-        break
-    elif calcs(option_a, option_b, user_selection) == "win":
-        score += 1
-        print(f" SCORE: {score}")
-
-
+        game = False
+        if user_selection == "a":
+            loser = option_a
+            winner = option_b
+        if user_selection == "b":
+            winner = option_a 
+            loser = option_b
+    # print the follower counts and the score
+    print(f"{winner['name']}: {winner['follower_count']}: {loser['name']}: {loser['follower_count']}: ")
+    print(f" SCORE: {score}")
